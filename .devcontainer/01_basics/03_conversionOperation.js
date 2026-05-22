f51f25b14f39831dfa143c6c8d82fcd138351251
Ankit Yadav <ankityadav2187201@gmail.com>
2026-05-22 13:26:56 +0000

    conversionOperation

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "70abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// "70" => 33 convert string to number easy
+// "70abc" => NaN (not a number) print
+// true => 1; false => 0;
+
+
+let isHello = 1
+
+
+let booleanIsHello = Boolean(isHello)
+
+console.log(typeof booleanIsHello);//boolean
+console.log(booleanIsHello);//true 
+
+
+// but "" is a false value in boolean type of data
