7daef0bd6cb452c21632bcaf5a3240f1d6ee31a1
Ankit Yadav <ankityadav2187201@gmail.com>
2026-05-22 07:21:26 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 325416
+let accountEmail = "example@gmail.com"
+var accountPassword = "4587265"
+accountCity = "New York"
+
+
+// accountId = 4152 // Not allowed because accountId is a constant variable
+
+
+
+accountEmail = "hello@gmail.com"
+accountPassword = "654321"
+accountCity = "Los Angeles"
+
+ console.log(accountId)
+
+//  prefers not to use variables because of issues block scope and function scope.
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
