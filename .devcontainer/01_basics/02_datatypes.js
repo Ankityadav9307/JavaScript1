312fcf2a7f2c933ac395e1d974f99394ad2b58ec
Ankit Yadav <ankityadav2187201@gmail.com>
2026-05-22 07:53:30 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,40 @@
+"use strict";// treat all js code as newer version
+
+
+//alert(3+3) // we are using nodejs , not browser
+
+
+let name = "Ankit"
+let age = 21
+let isboolean = true
+
+// console.log(isboolean)
+
+//  DataTypes
+
+
+//Number => 2 power 53
+
+// BigInt => using large number for example -- treding , stock market, big website
+
+// String => ""
+
+// Bollean => true/false
+
+// Null => standalone value // representation of empty value
+
+// Undefined => for example -- let state;  only declare to give undefined value
+
+// Symbol => unique
+
+
+
+// Object 
+
+console.log(typeof "Ankit");// String type of data
+console.log(typeof age); // number type of data
+console.log(typeof null); // object type of data
+console.log(typeof undefined); // undefined type of data
+
+
+
